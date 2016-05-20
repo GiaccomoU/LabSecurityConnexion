ae33b1aca66bb388323484787b7f79aa6faf34df
Giaccomo <giaccosup@gmail.com>
2016-05-19 18:44:17 -0600

    Initial commit

---
+++ app/src/main/java/com/example/giaccomo/securityconnexion/IngresoActivity.java
@@ -0,0 +1,22 @@
+package com.example.giaccomo.securityconnexion;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+public class IngresoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingreso);
+        Intent intent = getIntent();
+        String nombre = intent.getStringExtra("nombre");
+        String correo = intent.getStringExtra("correo");
+        TextView nombreTV = (TextView)
+                findViewById(R.id.textViewNombre);
+        TextView correoTV = (TextView)
+                findViewById(R.id.textViewCorreo);
+        nombreTV.setText(nombre);
+        correoTV.setText(correo);
+    }
+}
